76e31a7bf91684dcdb53fd328f967d64c2a96bf9
pedromello <pedro.renato.mello@gmail.com>
2026-02-02 11:59:26 -0300

    auth service with internal communication

---
+++ transactions-microservice/src/auth/strategies/internal-jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class InternalJwtStrategy extends PassportStrategy(Strategy, 'jwt-internal') {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: process.env.INTERNAL_JWT_SECRET || 'ILIACHALLENGE_INTERNAL',
+        });
+    }
+
+    async validate(payload: any) {
+        if (!payload.internal) {
+            throw new UnauthorizedException('Not an internal token');
+        }
+        return { internal: true };
+    }
+}
